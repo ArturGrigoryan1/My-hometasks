689932a08c8969ff3db907aa81a5ad552cb6fc7a
ArturGrigoryan1 <143454080+ArturGrigoryan1@users.noreply.github.com>
2024-03-01 16:08:58 +0400

    Update get_uniqes.py

--- Jenkins_hometasks/hometask_01_03_24/get_uniqes.py
+++ Jenkins_hometasks/hometask_01_03_24/get_uniqes.py
@@ -10,8 +10,8 @@ def get_uniques(mstr1,mstr2):
 
 
 def main():
-    mst1 = input("write a first string: ")
-    mst2 = input("write a second string: ")
+    mst1 = "the first string"
+    mst2 = "secondstr"
     print(get_uniques(mst1,mst2))
 if __name__ == '__main__':
     print("Hello world")
