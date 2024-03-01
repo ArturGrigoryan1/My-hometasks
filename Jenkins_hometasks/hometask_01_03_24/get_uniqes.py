d5af02b98f5a43b1c76302c7ceabaa501d49b5ed
Arthur <Arturishkhanich1998@gmail.com>
2024-03-01 15:09:37 +0400

    Begining of hometask_01_03_24

---
+++ Jenkins_hometasks/hometask_01_03_24/get_uniqes.py
@@ -0,0 +1,17 @@
+def get_uniques(mstr1,mstr2):
+    ms1 = ""
+    for el in mstr1:
+        if el not in mstr2:
+            ms1 += el
+    for el in mstr2:
+        if el not in mstr1:
+            ms1 += el
+    return ms1
+
+
+def main():
+    mst1 = input("write a first string: ")
+    mst2 = input('write a second string: ')
+    print(get_uniques(mst1,mst2))
+
+main()
